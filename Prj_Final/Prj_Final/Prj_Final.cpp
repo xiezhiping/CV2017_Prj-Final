0e80dfbef8cb9975e6621a59eff7809062512812
xiezhiping <1529464401@qq.com>
2018-01-26 16:00:35 +0800

    参考网上关于ORB提取特征算法，实现了基本的ORB特征提取和匹配，并在此基础上阅读Lowe论文进行了适当改进
    下一步是加入噪声图片，能对其进行分组并探索多张图片如何循环的进行ORB特征匹配
    
    
    
    本步骤匹配结果可在项目image文件夹下的match_result.jpg查看

--- Prj_Final/Prj_Final/Prj_Final.cpp
+++ Prj_Final/Prj_Final/Prj_Final.cpp
@@ -26,19 +26,71 @@ int main()
 	imgs.push_back(img);
 	img = imread(IMAGE_PATH_PREFIX + "boat2.jpg");
 	imgs.push_back(img);
-	img = imread(IMAGE_PATH_PREFIX + "boat3.jpg");
-	imgs.push_back(img);
-	img = imread(IMAGE_PATH_PREFIX + "boat3.jpg");
-	imgs.push_back(img);
-	img = imread(IMAGE_PATH_PREFIX + "boat4.jpg");
-	imgs.push_back(img);
-	img = imread(IMAGE_PATH_PREFIX + "boat5.jpg");
-	imgs.push_back(img);
-	img = imread(IMAGE_PATH_PREFIX + "boat6.jpg");
-	imgs.push_back(img);
+	//img = imread(IMAGE_PATH_PREFIX + "boat3.jpg");
+	//imgs.push_back(img);
+	//img = imread(IMAGE_PATH_PREFIX + "boat3.jpg");
+	//imgs.push_back(img);
+	//img = imread(IMAGE_PATH_PREFIX + "boat4.jpg");
+	//imgs.push_back(img);
+	//img = imread(IMAGE_PATH_PREFIX + "boat5.jpg");
+	//imgs.push_back(img);
+	//img = imread(IMAGE_PATH_PREFIX + "boat6.jpg");
+	//imgs.push_back(img);
+	for (int i = 0; i <imgs.size(); i++)
+	{
+		imshow("picture", imgs[i]);
+	}
+	//灰度图片转换
+	vector<Mat> cvt_imgs;
+	for (int i = 0; i < imgs.size(); i++)
+	{
+		Mat temp;
+		cvtColor(imgs[0], temp, CV_RGB2GRAY);
+		cvt_imgs.push_back(temp);
+	}
 
+	//提取特征点
+	OrbFeatureDetector orbFeatureDetector(1000);//参考opencvorb实现，调整精度，值越小，点越少越精准
+	vector< vector<KeyPoint>> keyPoints;
+	for (int i = 0; i < cvt_imgs.size(); i++)
+	{
+		vector<KeyPoint> keyPoint;
+		orbFeatureDetector.detect(cvt_imgs[i], keyPoint);
+		keyPoints.push_back(keyPoint);
+	}
+
+	//特征点描述，为特征点匹配做准备
+	OrbDescriptorExtractor orbDescriptor;
+	vector<Mat> imagesDesc;
+	for (int i = 0; i < cvt_imgs.size(); i++)
+	{
+		Mat imageDesc;
+		orbDescriptor.compute(cvt_imgs[i], keyPoints[i], imageDesc);
+		imagesDesc.push_back(imageDesc);
+	}
+	flann::Index flannIndex(imagesDesc[0], flann::LshIndexParams(12, 20, 2), cvflann::FLANN_DIST_HAMMING);
+	vector<DMatch> GoodMatchPoints;
+	Mat matchIndex(imagesDesc[1].rows, 2, CV_32SC1), matchDistance(imagesDesc[1].rows, 2, CV_32FC1);
+	flannIndex.knnSearch(imagesDesc[1], matchIndex, matchDistance, 2, flann::SearchParams());
+	
+
+	//采用Lowe's 算法选取优秀匹配点
+	for (int i = 0; i < matchDistance.rows; i++)
+	{
+		if (matchDistance.at<float>(i, 0) < 0.6*matchDistance.at<float>(i, 1))
+		{
+			DMatch dmatches(i, matchIndex.at<int>(i, 0), matchDistance.at<float>(i, 0));
+			GoodMatchPoints.push_back(dmatches);
+		}
+	}
+	Mat first_match;
+	drawMatches(imgs[1], keyPoints[1], imgs[0], keyPoints[0], GoodMatchPoints, first_match);
+	imshow("first_match", first_match);
+	string match_result = IMAGE_PATH_PREFIX + "match_result.jpg";
+	imwrite(match_result, first_match);
+	waitKey();
 	Mat pano;//拼接结果图片
-	//Stitcher stitcher = Stitcher::createDefault(try_use_gpu);
+	ORB orb;
 	Stitcher stitcher = Stitcher::createDefault(true);
 	Stitcher::Status status = stitcher.stitch(imgs, pano);
 
@@ -49,5 +101,6 @@ int main()
 	}
 
 	imwrite(result_name, pano);
+	waitKey();
 }
 

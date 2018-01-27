7637c1775b4754582c89b237b499ead0fc737eeb
xiezhiping <1529464401@qq.com>
2018-01-27 20:26:35 +0800

    今天学习理解并完善了图片校准、多张图片match分析、比较，将transform的图片进行合成并优化
    但是目前还有一个bug，应该是我图片位置导致的，还在排查中....
    
    
    下一步打算进行：
    
    bug调优
    
    多张图片删选以及拼接
    
    添加一定的交互
    
    完善相应实验效果分析

--- Prj_Final/Prj_Final/Prj_Final.cpp
+++ Prj_Final/Prj_Final/Prj_Final.cpp
@@ -8,23 +8,78 @@
 #include "opencv2/core/core.hpp"  
 #include<stdlib.h>
 #include <opencv2/stitching/stitcher.hpp>
-#include<opencv2/core/core.hpp>
+#include"opencv2/nonfree/nonfree.hpp"
+#include"opencv2/legacy/legacy.hpp"
 
 using namespace std;
 using namespace cv;
+typedef struct
+{
+	Point2f left_top;
+	Point2f left_bottom;
+	Point2f right_top;
+	Point2f right_bottom;
+}four_corners_t;
 
+void OptimizeSeam(Mat &img1, Mat &trans, Mat& dst);
+
+four_corners_t corners;
 string IMAGE_PATH_PREFIX = "./image/";
-
 bool try_use_gpu = false;
 vector<Mat> imgs;
 string result_name = IMAGE_PATH_PREFIX + "result.jpg";
 
+//计算图片四个角的值
+void CalcCorners(const Mat& H, const Mat& src)
+{
+	double v2[] = { 0, 0, 1 };//左上角
+	double v1[3];//变换后的坐标值
+	Mat V2 = Mat(3, 1, CV_64FC1, v2);  //列向量
+	Mat V1 = Mat(3, 1, CV_64FC1, v1);  //列向量
+
+	V1 = H * V2;
+	//左上角(0,0,1)
+	cout << "V2: " << V2 << endl;
+	cout << "V1: " << V1 << endl;
+	corners.left_top.x = v1[0] / v1[2];
+	corners.left_top.y = v1[1] / v1[2];
+
+	//左下角(0,src.rows,1)
+	v2[0] = 0;
+	v2[1] = src.rows;
+	v2[2] = 1;
+	V2 = Mat(3, 1, CV_64FC1, v2);  //列向量
+	V1 = Mat(3, 1, CV_64FC1, v1);  //列向量
+	V1 = H * V2;
+	corners.left_bottom.x = v1[0] / v1[2];
+	corners.left_bottom.y = v1[1] / v1[2];
+
+	//右上角(src.cols,0,1)
+	v2[0] = src.cols;
+	v2[1] = 0;
+	v2[2] = 1;
+	V2 = Mat(3, 1, CV_64FC1, v2);  //列向量
+	V1 = Mat(3, 1, CV_64FC1, v1);  //列向量
+	V1 = H * V2;
+	corners.right_top.x = v1[0] / v1[2];
+	corners.right_top.y = v1[1] / v1[2];
+
+	//右下角(src.cols,src.rows,1)
+	v2[0] = src.cols;
+	v2[1] = src.rows;
+	v2[2] = 1;
+	V2 = Mat(3, 1, CV_64FC1, v2);  //列向量
+	V1 = Mat(3, 1, CV_64FC1, v1);  //列向量
+	V1 = H * V2;
+	corners.right_bottom.x = v1[0] / v1[2];
+	corners.right_bottom.y = v1[1] / v1[2];
+}
 
 int main()
 {
-	Mat img = imread(IMAGE_PATH_PREFIX + "boat1.jpg");
+	Mat img = imread(IMAGE_PATH_PREFIX + "orb1.jpg");
 	imgs.push_back(img);
-	img = imread(IMAGE_PATH_PREFIX + "boat2.jpg");
+	img = imread(IMAGE_PATH_PREFIX + "orb2.jpg");
 	imgs.push_back(img);
 	//img = imread(IMAGE_PATH_PREFIX + "boat3.jpg");
 	//imgs.push_back(img);
@@ -83,6 +138,44 @@ int main()
 			GoodMatchPoints.push_back(dmatches);
 		}
 	}
+	//将goodmatch点集进行转换
+	vector<vector<Point2f>> Points2f;
+	vector<Point2f> imagePoint1, imagePoint2;
+	Points2f.push_back(imagePoint1);
+	Points2f.push_back(imagePoint2);
+	vector<KeyPoint> keyPoint1, keyPoint2;
+	keyPoint1=keyPoints[0];
+	keyPoint2 = keyPoints[1];
+		for (int i = 0; i < GoodMatchPoints.size(); i++)
+		{
+			Points2f[1].push_back(keyPoint2[GoodMatchPoints[i].queryIdx].pt);
+			Points2f[0].push_back(keyPoint1[GoodMatchPoints[i].trainIdx].pt);
+		}
+	//获取图像1到图像2 的投射矩阵，尺寸为3*3
+	Mat homo = findHomography(Points2f[0], Points2f[1], CV_RANSAC);//需要legacy.hpp头文件
+	cout << "变换矩阵为：\n" << homo << endl << endl;//输出映射矩阵
+
+	//计算配准图的四个坐标顶点
+	CalcCorners(homo, imgs[0]);
+
+	//图像匹配
+	vector<Mat> imagesTransform;
+	Mat imageTransform1, imageTransform2;
+	warpPerspective(imgs[0], imageTransform1, homo, Size(MAX(corners.right_top.x, corners.right_bottom.x), imgs[1].rows));
+	imagesTransform.push_back(imageTransform1);
+	imagesTransform.push_back(imageTransform2);
+	imshow("直接经过透视矩阵变换", imagesTransform[0]);
+	imwrite(IMAGE_PATH_PREFIX + "orb_transform_result.jpg", imageTransform1);
+
+	//图像拷贝
+	int dst_width = imagesTransform[0].cols;
+	int dst_height = imgs[1].rows;
+	Mat dst(dst_height, dst_width, CV_8UC3);
+	dst.setTo(0);
+	imagesTransform[0].copyTo(dst(Rect(0, 0, imagesTransform[0].cols, imagesTransform[0].rows)));
+	imgs[1].copyTo(dst(Rect(0, 0, imgs[1].cols, imgs[1].rows)));
+	imshow("copy_dst", dst);
+	imwrite(IMAGE_PATH_PREFIX + "copy_dst.jpg", dst);
 	Mat first_match;
 	drawMatches(imgs[1], keyPoints[1], imgs[0], keyPoints[0], GoodMatchPoints, first_match);
 	imshow("first_match", first_match);
@@ -102,5 +195,40 @@ int main()
 
 	imwrite(result_name, pano);
 	waitKey();
+}
+//优化两图的连接处，使得拼接自然
+void OptimizeSeam(Mat& img1, Mat& trans, Mat& dst)
+{
+	int start = MIN(corners.left_top.x, corners.left_bottom.x);//开始位置，即重叠区域的左边界  
+
+	double processWidth = img1.cols - start;//重叠区域的宽度  
+	int rows = dst.rows;
+	int cols = img1.cols; //注意，是列数*通道数
+	double alpha = 1;//img1中像素的权重  
+	for (int i = 0; i < rows; i++)
+	{
+		uchar* p = img1.ptr<uchar>(i);  //获取第i行的首地址
+		uchar* t = trans.ptr<uchar>(i);
+		uchar* d = dst.ptr<uchar>(i);
+		for (int j = start; j < cols; j++)
+		{
+			//如果遇到图像trans中无像素的黑点，则完全拷贝img1中的数据
+			if (t[j * 3] == 0 && t[j * 3 + 1] == 0 && t[j * 3 + 2] == 0)
+			{
+				alpha = 1;
+			}
+			else
+			{
+				//img1中像素的权重，与当前处理点距重叠区域左边界的距离成正比，实验证明，这种方法确实好  
+				alpha = (processWidth - (j - start)) / processWidth;
+			}
+
+			d[j * 3] = p[j * 3] * alpha + t[j * 3] * (1 - alpha);
+			d[j * 3 + 1] = p[j * 3 + 1] * alpha + t[j * 3 + 1] * (1 - alpha);
+			d[j * 3 + 2] = p[j * 3 + 2] * alpha + t[j * 3 + 2] * (1 - alpha);
+
+		}
+	}
+
 }
 
